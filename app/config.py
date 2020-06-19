aa742931456684030ad328bfe19cc323983bec1f
Matthew Tuusberg <tuusberg@gmail.com>
2020-06-19 13:03:21 +0300

    feat: disable mongodb authentication for simplicity

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from os import environ
 config = {
     'DB_HOST': environ['URL_SHORTENER_MONGODB_SERVICE_HOST'],
     'DB_PORT': int(environ['URL_SHORTENER_MONGODB_SERVICE_PORT']),
-    'DB_USER': 'root',
-    'DB_PASS': environ['MONGODB_PASSWORD']
+    # 'DB_USER': 'root',
+    # 'DB_PASS': environ['MONGODB_PASSWORD']
 }
