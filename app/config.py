7e196b4d71338a3dbc44baf56a7f2d393597ce1c
Matthew Tuusberg <tuusberg@gmail.com>
2020-06-19 11:59:40 +0300

    feat: use mongodb service private ip

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
 config = {
-    'DB_HOST': 'url-shortener-mongodb.default.svc.cluster.local',
+    'DB_HOST': environ['URL_SHORTENER_MONGODB_SERVICE_HOST'],
     'DB_PORT': int(environ['URL_SHORTENER_MONGODB_SERVICE_PORT']),
     'DB_USER': 'root',
     'DB_PASS': environ['MONGODB_PASSWORD']
