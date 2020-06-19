3715608c3b05c98c7c0b3640b7ed6a699b965e87
Matthew Tuusberg <tuusberg@gmail.com>
2020-06-19 11:24:46 +0300

    fix: cast port to integer type

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from os import environ
 
 config = {
     'DB_HOST': 'url-shortener-mongodb.default.svc.cluster.local',
-    'DB_PORT': environ['URL_SHORTENER_MONGODB_SERVICE_PORT'],
+    'DB_PORT': int(environ['URL_SHORTENER_MONGODB_SERVICE_PORT']),
     'DB_USER': 'root',
     'DB_PASS': environ['MONGODB_PASSWORD']
 }
